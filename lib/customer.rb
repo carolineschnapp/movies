8263fbeb7d4291dd628a4678acd1b02d8bbd1803
Caroline <mllegeorgesand@gmail.com>
2018-06-30 02:40:04 -0400

    Rename element -> rental

--- lib/customer.rb
+++ lib/customer.rb
@@ -16,16 +16,16 @@ class Customer
   def statement
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
-    @rentals.each do |element|
+    @rentals.each do |rental|
       # add frequent renter points
       frequent_renter_points += 1
       # add bonus for a two day new release rental
-      if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
+      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
           frequent_renter_points += 1
       end
       # show figures for this rental
-      result += "  " + element.movie.title + " " + element.charge.to_s + "\n"
-      total_amount += element.charge
+      result += "  " + rental.movie.title + " " + rental.charge.to_s + "\n"
+      total_amount += rental.charge
     end
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
