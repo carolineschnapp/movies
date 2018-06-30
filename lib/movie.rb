0311a4447cd09ae8748bb5862de76b3e6a35becb
Caroline <mllegeorgesand@gmail.com>
2018-06-30 09:05:27 -0400

    Continued (slowly)

--- lib/movie.rb
+++ lib/movie.rb
@@ -53,7 +53,9 @@ end
 class ChildrensPrice
   def charge(days_rented)
     result = 1.5
-    result += (days_rented - 3) * 1.5 if days_rented > 3
+    if days_rented > 3
+      result += (days_rented - 3) * 1.5
+    end
     result
   end
 end
