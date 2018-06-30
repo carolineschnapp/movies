42944eed9f77de2187bdb5b182f329e2acee07e0
Caroline <mllegeorgesand@gmail.com>
2018-06-30 09:03:16 -0400

    Use its returned value. Tests don't fail? YES.

--- lib/movie.rb
+++ lib/movie.rb
@@ -29,9 +29,7 @@ class Movie
     when NEW_RELEASE
       result = price.charge(days_rented)
     when CHILDRENS
-      price.charge(days_rented)
-      result += 1.5
-      result += (days_rented - 3) * 1.5 if days_rented > 3
+      result = price.charge(days_rented)
     end
     result
   end
