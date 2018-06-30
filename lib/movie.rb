08d263662f0bcd2d1771d7eb4bf8e97c8a2c7673
Caroline <mllegeorgesand@gmail.com>
2018-06-30 08:50:28 -0400

    Define a charge method in the RegularPrice strategy class and parse it

--- lib/movie.rb
+++ lib/movie.rb
@@ -42,6 +42,12 @@ class Movie
 end
 
 class RegularPrice
+  def charge(days_rented)
+    result = 0
+    result += 2
+    result += (days_rented - 2) * 1.5 if days_rented > 2
+    result
+  end
 end
 
 class NewReleasePrice
