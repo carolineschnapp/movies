814b04e48ddba3bfa8081cabfff18a3717415b19
Caroline <mllegeorgesand@gmail.com>
2018-06-30 02:51:24 -0400

    Recognizing Temp variables, let's replace by queries, so call to methods (step 1)

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,15 +14,21 @@ class Customer
   end
 
   def statement
-    total_amount, frequent_renter_points = 0, 0
-    result = "Rental Record for #{@name}\n"
+    total_amount = 0
     @rentals.each do |rental|
-      # add frequent renter points
-      frequent_renter_points += rental.frequent_renter_points
-      # show figures for this rental
-      result += "  " + rental.movie.title + " " + rental.charge.to_s + "\n"
       total_amount += rental.charge
     end
+
+    frequent_renter_points = 0
+    @rentals.each do |rental|
+      frequent_renter_points += rental.frequent_renter_points
+    end
+
+    result = "Rental Record for #{@name}\n"
+    @rentals.each do |rental|
+      result += "  " + rental.movie.title + " " + rental.charge.to_s + "\n"
+    end
+
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
