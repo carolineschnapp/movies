53970d44ce7afc2cb141430f9e5f3f54c3221166
Caroline <mllegeorgesand@gmail.com>
2018-06-30 08:38:48 -0400

    Edit initializer to help us apply 'Replace Type Code with State/Strategy'

--- lib/movie.rb
+++ lib/movie.rb
@@ -7,7 +7,8 @@ class Movie
   attr_accessor :price_code
 
   def initialize(title, price_code)
-    @title, @price_code = title, price_code
+    @title = title
+    @price_code = price_code
   end
 
   def charge(days_rented)
