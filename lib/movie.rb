0d01d85c3155a2d008be1d9d16653e7049ac5198
Caroline <mllegeorgesand@gmail.com>
2018-06-30 09:04:46 -0400

    Start refactoring ChildrensPrice#charge

--- lib/movie.rb
+++ lib/movie.rb
@@ -52,8 +52,7 @@ end
 
 class ChildrensPrice
   def charge(days_rented)
-    result = 0
-    result += 1.5
+    result = 1.5
     result += (days_rented - 3) * 1.5 if days_rented > 3
     result
   end
