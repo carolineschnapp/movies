f2b2befe5741eacfd091ab3eb6a926c1c9d0e64c
Caroline <mllegeorgesand@gmail.com>
2018-06-30 02:55:33 -0400

    Name the query/method after the temp variable

--- lib/customer.rb
+++ lib/customer.rb
@@ -14,16 +14,6 @@ class Customer
   end
 
   def statement
-    total_amount = 0
-    @rentals.each do |rental|
-      total_amount += rental.charge
-    end
-
-    frequent_renter_points = 0
-    @rentals.each do |rental|
-      frequent_renter_points += rental.frequent_renter_points
-    end
-
     result = "Rental Record for #{@name}\n"
     @rentals.each do |rental|
       result += "  " + rental.movie.title + " " + rental.charge.to_s + "\n"
@@ -35,18 +25,19 @@ class Customer
     result += "\n"
   end
 
-  def charge(rental)
-    this_amount = 0
-    case rental.movie.price_code
-    when Movie::REGULAR
-      this_amount += 2
-      this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
-    when Movie::NEW_RELEASE
-      this_amount += rental.days_rented * 3
-    when Movie::CHILDRENS
-      this_amount += 1.5
-      this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+  def total_amount
+    total_amount = 0
+    @rentals.each do |rental|
+      total_amount += rental.charge
     end
-    this_amount
+    total_amount
+  end
+
+  def frequent_renter_points
+    frequent_renter_points = 0
+    @rentals.each do |rental|
+      frequent_renter_points += rental.frequent_renter_points
+    end
+    frequent_renter_points
   end
 end
