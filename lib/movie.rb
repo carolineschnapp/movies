f4543a941a9107f1a08131d432fa3dfc883dcecf
Caroline <mllegeorgesand@gmail.com>
2018-06-30 09:15:37 -0400

    Create module to mix-in a frequent_renter_points method

--- lib/movie.rb
+++ lib/movie.rb
@@ -35,7 +35,15 @@ class Movie
   end
 end
 
+module DefaultPrice
+  def frequent_renter_points(days_rented)
+    1
+  end
+end
+
 class RegularPrice
+  include DefaultPrice
+
   def charge(days_rented)
     days_rented > 2 ? days_rented * 1.5 - 1 : 2
   end
@@ -45,9 +53,15 @@ class NewReleasePrice
   def charge(days_rented)
     days_rented * 3
   end
+
+  def frequent_renter_points(days_rented)
+    days_rented > 1 ? 2 : 1
+  end
 end
 
 class ChildrensPrice
+  include DefaultPrice
+
   def charge(days_rented)
     days_rented > 3 ? days_rented * 1.5 - 3 : 1.5
   end
