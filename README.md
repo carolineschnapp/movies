0776e4b261175fd85f622d0354a4643b694c9e28
Caroline <mllegeorgesand@gmail.com>
2018-06-29 23:57:35 -0400

    Code good enough for tests to pass

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # Refactoring Ruby Edition
 
-This is work done while reading the book Refactoring Ruby Edition. 
+This is work done while reading the book Refactoring Ruby Edition.
 
 Every 1-line change is 1 commit.
 
 The branch `chapter-1` contains the work done on chapter 1, etc.
+
+## Tips from the book
+
+### Chapter 1
+
+“When you find you have to add a feature to a program, and the program’s code is not structured in a convenient way to add the feature, first refactor the program to make it easy to add the feature, then add the feature.”
+
+“Before you start refactoring, check that you have a solid suite of tests. These tests must be self-checking.”
---
+++ test/movie_rental_test.rb
@@ -0,0 +1,75 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/customer'
+require_relative '../lib/movie'
+require_relative '../lib/rental'
+
+class MovieRentalTest < Minitest::Test
+  def setup
+    @customer = Customer.new('Caroline')
+    @children_movie = Movie.new('Ratatouille', Movie::CHILDRENS)
+    @regular_movie = Movie.new('The Big Lebowski', Movie::REGULAR)
+    @new_release_movie = Movie.new('Frantz', Movie::NEW_RELEASE)
+  end
+
+  def test_movie_has_title
+    assert_equal 'Ratatouille', @children_movie.title
+  end
+
+  def test_movie_has_price_code
+    assert_equal Movie::CHILDRENS, @children_movie.price_code
+  end
+
+  def test_movie_has_price_code_that_can_be_changed
+    @children_movie.price_code = Movie::NEW_RELEASE
+    assert_equal Movie::NEW_RELEASE, @children_movie.price_code
+  end
+
+  def test_customer_has_name
+    assert_equal 'Caroline', @customer.name
+  end
+
+  def test_customer_can_be_given_a_statement_for_one_rental
+    @customer.add_rental(Rental.new(@new_release_movie, 3))
+    expected_statement = <<~STATEMENT
+      Rental Record for Caroline
+        Frantz 9
+      Amount owed is 9
+      You earned 2 frequent renter points
+    STATEMENT
+    assert_equal expected_statement, @customer.statement
+  end
+
+  def test_customer_can_be_given_a_statement_for_several_rentals
+    @customer.add_rental(Rental.new(@children_movie, 2))
+    @customer.add_rental(Rental.new(@regular_movie, 3))
+    @customer.add_rental(Rental.new(@new_release_movie, 1))
+    expected_statement = <<~STATEMENT
+      Rental Record for Caroline
+        Ratatouille 1.5
+        The Big Lebowski 3.5
+        Frantz 3
+      Amount owed is 8.0
+      You earned 3 frequent renter points
+    STATEMENT
+    assert_equal expected_statement, @customer.statement
+  end
+
+  def test_customer_can_be_given_a_statement_in_html
+    skip
+    @customer.add_rental(Rental.new(@children_movie, 2))
+    @customer.add_rental(Rental.new(@regular_movie, 3))
+    @customer.add_rental(Rental.new(@new_release_movie, 1))
+    expected_html_statement = <<~STATEMENT
+      <h1>Rental Record for Caroline</h1>
+      <ul>
+        <li>Ratatouille 1.5</li>
+        <li>The Big Lebowski 3.5</li>
+        <li>Frantz 3</li>
+      </ul>
+      <p>Amount owed is 8.0</p>
+      <p>You earned 3 frequent renter points</p>
+    STATEMENT
+    assert_equal expected_html_statement, @customer.html_statement
+  end
+end
