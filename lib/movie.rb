3512670ee163336b9ce2448e948b71605807fc23
Caroline <mllegeorgesand@gmail.com>
2018-06-30 08:55:56 -0400

    Continued

--- lib/movie.rb
+++ lib/movie.rb
@@ -44,7 +44,9 @@ end
 class RegularPrice
   def charge(days_rented)
     result = 2
-    result += (days_rented - 2) * 1.5 if days_rented > 2
+    if days_rented > 2
+       result += (days_rented - 2) * 1.5
+    end
     result
   end
 end
